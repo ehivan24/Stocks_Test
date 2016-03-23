0b9ae21562de97eae5e8315d7e7463cc448e7da4
Hivan <ehivan24@hotmail.com>
2016-03-23 14:01:24 -0400

    Converted to non static
    /**
    	 * Thread Safety -  resources are by definition not thread-safe
    	 *	Code Implicity - You do not know when a  variables is instantiated
    	 *  and whether or not it will be instantiated before another  variable
    	 */

--- RunTest.java
+++ RunTest.java
@@ -1,38 +1,41 @@
-import java.awt.Color;
+package Stocks_Test;
 
 public class RunTest {
-	public static void main(String[] args){
-		TestOne.setStockName("APPL"); //json Symbol
-		TestOne.setChangeFromYearHigh(-28.20);
-		TestOne.setChangeFromYearLow(31.20);
-		TestOne.setCurrentPrice(146.16);
-		TestOne.setYearHigh(176.30);
-		TestOne.setYearLow(116.90);
-		TestOne.setCurrentPrice(123.0);
-		TestOne.setPreviousClose(148.10);
-		TestOne.setFiftyMovingAverage(133.88);
-		TestOne.setTwoHundredFiftyMovingAverage(137.75);
-		TestOne.setOneyrTargetPrice(131.57);
-		
-		String STOCK_NAME = TestOne.getStockName();
-		double CHANGE_FROM_YEAR_HIGH = TestOne.getChangeFromYearHigh();
-		double CHANGE_FROM_YEAR_LOW = TestOne.getChangeFromYearLow();
-		double CURRENT_PRICE = TestOne.getCurrentPrice();
-		double GET_YEAR_HIGH = TestOne.getYearHigh();
-		double GET_YEAR_LOW = TestOne.getYearLow();
-		double GET_OPEN_PRICE = TestOne.getOpenPrice();
-		double GET_PREVIOUS_CLOSE_PRICE =  TestOne.getPreviousClose();
-		double GET_FIFTY_AVERAGE_MOVING = TestOne.getFiftyMovingAverage();
-		double GET_TWo_HUNDRED_FIFTY_AVERAGE_MOVING = TestOne.getTwoHundredFiftyMovingAverage();
-		double SET_ONE_YEAR_PRICE = TestOne.getOneyrTargetPrice();
 	
-		System.out.println("50 Day Move: " + TestOne.FiftydayMovingAverage(CURRENT_PRICE, GET_FIFTY_AVERAGE_MOVING));
-		System.out.println("250 Day Move: " + TestOne.TwoHundreddayMovingAverage(CURRENT_PRICE, GET_TWo_HUNDRED_FIFTY_AVERAGE_MOVING));
-		System.out.println("Average Price Per Day: " + TestOne.averagePricePerday(GET_OPEN_PRICE, CURRENT_PRICE));
-		System.out.println("Target Price: " + TestOne.oneyrTargetPrice(CURRENT_PRICE, SET_ONE_YEAR_PRICE));
-		System.out.println("Cuurent Price: " + TestOne.getCurrentPrice());
+	public static void main(String[] args){
+		TestOne testOneObj = new TestOne();
+		
+		testOneObj.setStockName("APPL"); //json Symbol
+		testOneObj.setChangeFromYearHigh(-28.20);
+		testOneObj.setChangeFromYearLow(31.20);
+		testOneObj.setCurrentPrice(146.16);
+		testOneObj.setYearHigh(176.30);
+		testOneObj.setYearLow(116.90);
+		testOneObj.setCurrentPrice(123.0);
+		testOneObj.setPreviousClose(148.10);
+		testOneObj.setFiftyMovingAverage(133.88);
+		testOneObj.setTwoHundredFiftyMovingAverage(137.75);
+		testOneObj.setOneyrTargetPrice(131.57);
+		
+		String STOCK_NAME = testOneObj.getStockName();
+		double CHANGE_FROM_YEAR_HIGH = testOneObj.getChangeFromYearHigh();
+		double CHANGE_FROM_YEAR_LOW = testOneObj.getChangeFromYearLow();
+		double CURRENT_PRICE = testOneObj.getCurrentPrice();
+		double GET_YEAR_HIGH = testOneObj.getYearHigh();
+		double GET_YEAR_LOW = testOneObj.getYearLow();
+		double GET_OPEN_PRICE = testOneObj.getOpenPrice();
+		double GET_PREVIOUS_CLOSE_PRICE =  testOneObj.getPreviousClose();
+		double GET_FIFTY_AVERAGE_MOVING = testOneObj.getFiftyMovingAverage();
+		double GET_TWo_HUNDRED_FIFTY_AVERAGE_MOVING = testOneObj.getTwoHundredFiftyMovingAverage();
+		double SET_ONE_YEAR_PRICE = testOneObj.getOneyrTargetPrice();
+	
+		System.out.println("50 Day Move: " + testOneObj.FiftydayMovingAverage(CURRENT_PRICE, GET_FIFTY_AVERAGE_MOVING));
+		System.out.println("250 Day Move: " + testOneObj.TwoHundreddayMovingAverage(CURRENT_PRICE, GET_TWo_HUNDRED_FIFTY_AVERAGE_MOVING));
+		System.out.println("Average Price Per Day: " + testOneObj.averagePricePerday(GET_OPEN_PRICE, CURRENT_PRICE));
+		System.out.println("Target Price: " + testOneObj.oneyrTargetPrice(CURRENT_PRICE, SET_ONE_YEAR_PRICE));
+		System.out.println("Cuurent Price: " + testOneObj.getCurrentPrice());
 		System.out.println( STOCK_NAME +" High: " + GET_YEAR_HIGH + " Low: " + GET_YEAR_LOW + " Price: " + CURRENT_PRICE);
 		System.out.println( STOCK_NAME +" Year High: " + CHANGE_FROM_YEAR_HIGH + " Year Low: " + CHANGE_FROM_YEAR_LOW + " Last Price: " + GET_PREVIOUS_CLOSE_PRICE);
-		TestOne.stockStatus();
+		System.out.println("Status: " + testOneObj.stockStatus());
 	}
 }
--- TestOne.java
+++ TestOne.java
@@ -1,108 +1,118 @@
+package Stocks_Test;
+
 public class TestOne {
-	static String stockName;
-	static double currentPrice;
-	static double yearLow;
-	static double yearHigh;
-	static double changeFromYearLow;
-	static double changeFromYearHigh;
-	static double openPrice;
-	static double previousClose;
-	static double fiftyMovingAverage;
-	static double twoHundredFiftyMovingAverage;
-	static double oneyrTargetPrice;
+	/**
+	 * Thread Safety -  resources are by definition not thread-safe
+	 *	Code Implicity - You do not know when a  variables is instantiated 
+	 *  and whether or not it will be instantiated before another  variable
+	 */
 	
-	public static void stockStatus(){
-		if(TestOne.averagePricePerday(currentPrice, openPrice) < TestOne.getCurrentPrice()){
-			System.out.println("-" + currentPrice);
+	 String stockName;
+	 double currentPrice;
+	 double yearLow;
+	 double yearHigh;
+	 double changeFromYearLow;
+	 double changeFromYearHigh;
+	 double openPrice;
+	 double previousClose;
+	 double fiftyMovingAverage;
+	 double twoHundredFiftyMovingAverage;
+	 double oneyrTargetPrice;
+	
+	public  double stockStatus(){
+		double difference = this.getCurrentPrice() - this.getPreviousClose();
+		if(this.previousClose < this.getCurrentPrice()){
+			System.out.println(difference);
 		}else{
-			System.out.println("+" + currentPrice);
+			System.out.println(difference);
 		}
+		return difference;
 	}
 	
-	public static double FiftydayMovingAverage(double currentStockPrice, double priceFiftyDays){
+	public  double FiftydayMovingAverage(double currentStockPrice, double priceFiftyDays){
 		double meanAverage = currentStockPrice - priceFiftyDays;  
 		return meanAverage;
 	}
 	
-	public static double TwoHundreddayMovingAverage(double currentStockPrice, double priceTwoHundredFiftyDays){
+	public  double TwoHundreddayMovingAverage(double currentStockPrice, double priceTwoHundredFiftyDays){
 		double meanAverage = currentStockPrice - priceTwoHundredFiftyDays;  
 		return meanAverage;
 	}
 	
-	public static double averagePricePerday(double openPrice, double currentPrice){
+	public  double averagePricePerday(double openPrice, double currentPrice){
 		double meanPrice = currentPrice - openPrice;
 		return meanPrice;
 	}
 	
-	public static double oneyrTargetPrice(double currentPrice, double projectedPrice) {
+	public  double oneyrTargetPrice(double currentPrice, double projectedPrice) {
 		double price = currentPrice - projectedPrice;
 		return price;
 	}
-	public static double getCurrentPrice() {
+	public  double getCurrentPrice() {
 		return currentPrice;
 	}
-	public static void setCurrentPrice(double currentPrice) {
-		TestOne.currentPrice = currentPrice;
+	public  void setCurrentPrice(double currentPrice) {
+		this.currentPrice = currentPrice;
 	}
-	public static double getYearLow() {
+	public  double getYearLow() {
 		return yearLow;
 	}
-	public static void setYearLow(double yearLow) {
-		TestOne.yearLow = yearLow;
+	public  void setYearLow(double yearLow) {
+		this.yearLow = yearLow;
 	}
-	public static double getYearHigh() {
+	public  double getYearHigh() {
 		return yearHigh;
 	}
-	public static void setYearHigh(double yearHigh) {
-		TestOne.yearHigh = yearHigh;
+	public  void setYearHigh(double yearHigh) {
+		this.yearHigh = yearHigh;
 	}
-	public static double getChangeFromYearLow() {
+	public  double getChangeFromYearLow() {
 		return changeFromYearLow;
 	}
-	public static void setChangeFromYearLow(double changeFromYearLow) {
-		TestOne.changeFromYearLow = changeFromYearLow;
+	public  void setChangeFromYearLow(double changeFromYearLow) {
+		this.changeFromYearLow = changeFromYearLow;
 	}
-	public static double getChangeFromYearHigh() {
+	public  double getChangeFromYearHigh() {
 		return changeFromYearHigh;
 	}
-	public static void setChangeFromYearHigh(double changeFromYearHigh) {
-		TestOne.changeFromYearHigh = changeFromYearHigh;
+	public  void setChangeFromYearHigh(double changeFromYearHigh) {
+		this.changeFromYearHigh = changeFromYearHigh;
 	}
-	public static double getOpenPrice() {
+	public  double getOpenPrice() {
 		return openPrice;
 	}
-	public static void setOpenPrice(double openPrice) {
-		TestOne.openPrice = openPrice;
+	public  void setOpenPrice(double openPrice) {
+		this.openPrice = openPrice;
 	}
-	public static String getStockName() {
+	public  String getStockName() {
 		return stockName;
 	}
-	public static void setStockName(String stockName) {
-		TestOne.stockName = stockName;
+	public  void setStockName(String stockName) {
+		this.stockName = stockName;
 	}
-	public static double getPreviousClose() {
+	public  double getPreviousClose() {
 		return previousClose;
 	}
-	public static void setPreviousClose(double previousClose) {
-		TestOne.previousClose = previousClose;
+	public  void setPreviousClose(double previousClose) {
+		this.previousClose = previousClose;
 	}
-	public static double getFiftyMovingAverage() {
+	public  double getFiftyMovingAverage() {
 		return fiftyMovingAverage;
 	}
-	public static void setFiftyMovingAverage(double fiftyMovingAverage) {
-		TestOne.fiftyMovingAverage = fiftyMovingAverage;
+	public  void setFiftyMovingAverage(double fiftyMovingAverage) {
+		this.fiftyMovingAverage = fiftyMovingAverage;
 	}
-	public static double getTwoHundredFiftyMovingAverage() {
+	public  double getTwoHundredFiftyMovingAverage() {
 		return twoHundredFiftyMovingAverage;
 	}
-	public static void setTwoHundredFiftyMovingAverage(double twoHundredFiftyMovingAverage) {
-		TestOne.twoHundredFiftyMovingAverage = twoHundredFiftyMovingAverage;
+	public  void setTwoHundredFiftyMovingAverage(double twoHundredFiftyMovingAverage) {
+		this.twoHundredFiftyMovingAverage = twoHundredFiftyMovingAverage;
 	}
-	public static double getOneyrTargetPrice() {
+	public  double getOneyrTargetPrice() {
 		return oneyrTargetPrice;
 	}
-	public static void setOneyrTargetPrice(double oneyrTargetPrice) {
-		TestOne.oneyrTargetPrice = oneyrTargetPrice;
+	public  void setOneyrTargetPrice(double oneyrTargetPrice) {
+		this.oneyrTargetPrice = oneyrTargetPrice;
 	}
 	
 }
